54d63519501ca613096b89046e2046a60385cf49
gfouzie <gfouzie@uwaterloo.ca>
2022-07-13 23:10:27 -0400

    changed preview text in experience section

--- src/components/experience/data.ts
+++ src/components/experience/data.ts
@@ -2,9 +2,9 @@ export const kontek = {
   company: "Kontek Ecology",
   title: "Fullstack Developer",
   description:
-    "Contributed in the creation of Kontek’s  first client-facing web app which recorded live data from projects and displayed it to clients through customizable graphs, charts, and other widgets. Tech: AngularJS, Express.js, Node, REST APIs, MYSQL, Postgres, Docker.",
+    "Contributed in the creation of Kontek’s first client-facing web app which recorded live data from projects and displayed it to clients through customizable graphs, charts, and other widgets. Tech: AngularJS, Express.js, Node, REST APIs, MYSQL, Postgres, Docker.",
   link: "https://www.kontekwater.com/",
-  preview: "I helped to create Kontek's first client-facing...",
+  preview: "Contributed in the creation of Kontek’s first ...",
 };
 
 export const vivid = {
@@ -13,7 +13,7 @@ export const vivid = {
   description:
     "Currently collaborate with a large team to develop new features and optimize existing code to provide an engaging user experience attracting over a hundred million annual visits. Tech: ReactJS, TS, Strapi, MySQL, Jenkins, Jest, Next.js, Docker, YAML.",
   link: "https://www.vividseats.com/",
-  preview: "I currently work in a large team that...",
+  preview: "Currently collaborate with a large team to...",
 };
 
 export const changeroom = {
@@ -21,7 +21,7 @@ export const changeroom = {
   title: "COO",
   description: `Co-founded the SaaS startup and guided a small team of software developers to winning the Ottawa Business Journal’s “Best New Ottawa Business, 2019”  Tech: The original web application was made using a MERN stack, but it was later transformed into a React Native app.`,
   link: "https://changeroom.io/",
-  preview: "In 2019, I co-founded the SaaS startup...",
+  preview: "Co-founded the SaaS startup and guided...",
 };
 
 export const cryptoparrot = {
@@ -30,5 +30,5 @@ export const cryptoparrot = {
   description:
     "Researched various cryptocurrencies and wrote brief rundowns for a social trading simulator created to help users learn about digital currencies, before investing their own in-game currency.",
   link: "https://cryptoparrot.com/",
-  preview: "At Crypto Parrot, I researched various...",
+  preview: "Researched various cryptocurrencies and wrote...",
 };
